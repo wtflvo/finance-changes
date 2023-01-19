11c5b01bfce76acf943131d1ce9bc1c4cd488000
Viacheslav <109350140+wtflvo@users.noreply.github.com>
2023-01-19 04:16:52 +0200

    v.1.1.1

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import { IntervalBtn } from "./components/IntervalForm";
 import { TickersDashboard } from "./components/TickersDashboard";
-
+import { Header } from "./components/Header";
 import GetData from "./services/getTickersData";
 
 function App() {
 	return (
 		<div className="App">
+			<Header/>
 			<TickersDashboard />
 			<IntervalBtn/>
 			<GetData />
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,7 @@
+import "./index.css";
+
+export function Header(){
+    return <div className="header">
+        <h1>Tickers</h1>
+    </div>
+}
